38a1def6b93dbb27816b23252074135ac798adb0
gerpea <german.tcyganov@gmail.com>
2020-12-10 12:19:45 -0500

    ellipse renderer

--- Graph/utils/src/index.ts
+++ Graph/utils/src/index.ts
@@ -3,33 +3,51 @@ import { Pea } from './base/pea'
 import { PeaEngine } from './base/peaEngine'
 import { ChangeAngle } from './scripts/changeAngle'
 import { ChangeCenter } from './scripts/changeCenter'
+import { EllipseRenderer } from './scripts/ellipseRenderer'
 import { Move } from './scripts/move'
 import { RectRenderer } from './scripts/rectRenderer'
 
 const canvas: HTMLCanvasElement = document.getElementById('canvas') as HTMLCanvasElement
 const context: CanvasRenderingContext2D = canvas.getContext('2d') as CanvasRenderingContext2D
+const { width, height } = canvas.getBoundingClientRect()
 
 const peaEngine = new PeaEngine()
 
-const firstPea = new Pea('pea1', new Point(250, 150), new Point(0, 0), { angle: Math.PI })
-const secondPea = new Pea('pea2', new Point(150, 150), new Point(-25, 0), { angle: Math.PI / 4 })
+const background = new Pea('background', new Point(width / 2, height / 2), new Point(0, 0), {
+  angle: Math.PI * 2,
+})
+
+const firstPea = new Pea('pea1', new Point(0, 0), new Point(0, 0), {
+  angle: Math.PI,
+})
+const secondPea = new Pea('pea2', new Point(0, 0), new Point(0, 0), { angle: 0 })
 const thirdPea = new Pea('pea3', new Point(250, 150), new Point(0, 0), { angle: Math.PI * 2 })
 const fourthPea = new Pea('pea4', new Point(350, 150), new Point(0, 0), { angle: Math.PI / 4 })
 const fifthPea = new Pea('pea5', new Point(0, 0), new Point(0, 0), { angle: Math.PI / 4 })
 
 firstPea.addChild(fifthPea)
+fifthPea.addChild(secondPea)
 
+peaEngine.addPea(background)
 peaEngine.addPea(firstPea)
 peaEngine.addPea(fifthPea)
+peaEngine.addPea(secondPea)
 
-firstPea.addScript(new RectRenderer(context, 300, 300, new Color(255, 0, 0, 1)))
+background.addScript(new RectRenderer(context, width, height, new Color(127, 127, 127, 1)))
+firstPea.addScript(new RectRenderer(context, width / 2, height / 2, new Color(255, 0, 0, 1)))
 fifthPea.addScript(new RectRenderer(context, 30, 30, new Color(0, 100, 255, 1)))
 
-firstPea.addScript(new Move(new Point(250, 150), new Point(250, 250), new Point(0.1, 0.1)))
+firstPea.addScript(
+  new Move(
+    new Point(width / 2, height / 4),
+    new Point(width / 2, height / (4 / 3)),
+    new Point(0, 0.5)
+  )
+)
 fifthPea.addScript(new ChangeAngle())
 fifthPea.addScript(new Move(new Point(-50, 0), new Point(50, 0), new Point(0.1, 0.1)))
 
-secondPea.addScript(new RectRenderer(context, 50, 50, new Color(0, 0, 255, 1)))
+secondPea.addScript(new EllipseRenderer(context, 50, 50, new Color(0, 122, 122, 1)))
 thirdPea.addScript(new RectRenderer(context, 50, 50, new Color(0, 0, 255, 1)))
 fourthPea.addScript(new RectRenderer(context, 50, 50, new Color(0, 0, 255, 1)))
 
--- Graph/utils/src/scripts/ellipseRenderer.ts
+++ Graph/utils/src/scripts/ellipseRenderer.ts
@@ -1,6 +1,6 @@
 import { Script, Point, Color } from '../base'
 
-export class RectRenderer extends Script {
+export class EllipseRenderer extends Script {
   protected context: CanvasRenderingContext2D
 
   private halfPea: Point
@@ -31,27 +31,16 @@ export class RectRenderer extends Script {
 
     this.context.fillStyle = `rgba(${this.color.r}, ${this.color.g}, ${this.color.b}, ${this.color.a})`
 
-    this.context.moveTo(position.x - this.width / 2, position.y - this.height / 2)
-
     this.context.beginPath()
-    const lb = position.plus(
-      new Point(-this.halfPea.x, -this.halfPea.y).rotate(this.pea.center, this.pea.rotation)
+    this.context.ellipse(
+      position.x,
+      position.y,
+      this.width / 2,
+      this.height / 2,
+      this.pea.rotation.angle,
+      Math.PI * 2,
+      Math.PI
     )
-    const lt = position.plus(
-      new Point(-this.halfPea.x, this.halfPea.y).rotate(this.pea.center, this.pea.rotation)
-    )
-    const rt = position.plus(
-      new Point(this.halfPea.x, this.halfPea.y).rotate(this.pea.center, this.pea.rotation)
-    )
-    const rb = position.plus(
-      new Point(this.halfPea.x, -this.halfPea.y).rotate(this.pea.center, this.pea.rotation)
-    )
-
-    this.context.lineTo(lb.x, lb.y)
-    this.context.lineTo(lt.x, lt.y)
-    this.context.lineTo(rt.x, rt.y)
-    this.context.lineTo(rb.x, rb.y)
-
     this.context.closePath()
     this.context.fill()
 
