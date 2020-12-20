5690077ba4c56e359f9d20ea1dfe01028caa42b9
gerpea <german.tcyganov@gmail.com>
2020-12-20 05:25:15 -0500

    rewrite algorithm

---
+++ Graph/utils/graph/src/algorithm/eulerian.ts
@@ -0,0 +1,84 @@
+import { Graph, GraphEdge, GraphNode } from '../gra'
+
+function eulerianPath(graph: Graph): Array<GraphNode> {
+  let path: Array<GraphNode> = []
+  let visitedEdges = new Set<GraphEdge>()
+  const nodes = graph.getNodes()
+
+  if (!hasEulerianPath(nodes)) {
+    return path
+  }
+
+  let currentNode = findStartNode(nodes)!
+  while (visitedEdges.size !== graph.edgesCount) {
+    path.push(currentNode)
+    const edge = graph.getEdges(currentNode).find((edge) => !visitedEdges.has(edge))
+    if (edge?.node) {
+      visitedEdges.add(edge)
+      if (!edge.directed) {
+        visitedEdges.add(graph.getEdges(edge.node).find((e) => e.node.isEqual(currentNode))!)
+      }
+      currentNode = edge.node
+    } else {
+      break
+    }
+  }
+  path.push(currentNode)
+
+  return path
+}
+
+function eulerianCycle(graph: Graph): Array<GraphNode> {
+  if (!hasEulerianCycle(graph.getNodes())) {
+    return []
+  }
+
+  return eulerianPath(graph)
+}
+
+function hasEulerianPath(nodes: Array<GraphNode>): boolean {
+  let startNodes = 0
+  let endNodes = 0
+
+  for (let node of nodes) {
+    if (Math.abs(node.inDegree - node.outDegree) > 1) {
+      return false
+    } else if (node.inDegree - node.outDegree === 1) {
+      endNodes++
+    } else if (node.outDegree - node.inDegree === 1) {
+      startNodes++
+    }
+  }
+
+  return (
+    nodes.length > 0 &&
+    ((startNodes === 0 && endNodes === 0) || (startNodes === 1 && endNodes === 1))
+  )
+}
+
+function hasEulerianCycle(nodes: Array<GraphNode>): boolean {
+  for (let node of nodes) {
+    if (Math.abs(node.inDegree - node.outDegree) !== 0) {
+      return false
+    }
+  }
+
+  return nodes.length > 0
+}
+
+function findStartNode(nodes: Array<GraphNode>): GraphNode | undefined {
+  let start = undefined
+
+  for (let node of nodes) {
+    if (node.outDegree - node.inDegree === 1) {
+      return node
+    }
+    if (node.outDegree > 0) {
+      start = node
+    }
+  }
+
+  return start
+}
+
+export { eulerianPath, eulerianCycle }
---
+++ Graph/utils/graph/tests/algorithm/Eulerian.test.ts
@@ -0,0 +1,114 @@
+import 'mocha'
+import { expect } from 'chai'
+
+import { Graph, GraphNode } from '../../src/gra'
+import { eulerianPath, eulerianCycle } from '../../src/algorithm/eulerian'
+
+describe('Eulerian', function () {
+  let graph: Graph
+  beforeEach(function () {
+    graph = new Graph()
+  })
+
+  describe('Path', function () {
+    it('Should return empty path if graph is empty', function () {
+      const expected: Array<GraphNode> = []
+
+      const result = eulerianPath(graph)
+
+      expect(result).deep.equal(expected)
+    })
+
+    it('Should return empty path when graph does not has eulerian path', function () {
+      const expected: Array<GraphNode> = []
+      graph.addEdge('Node_1', 'Node_2', {
+        directed: true,
+      })
+      graph.addEdge('Node_1', 'Node_3')
+      graph.addEdge('Node_4', 'Node_3', {
+        directed: true,
+      })
+      graph.addEdge('Node_4', 'Node_2', {
+        directed: true,
+      })
+
+      const result = eulerianPath(graph)
+
+      expect(result).deep.equal(expected)
+    })
+
+    it('Should return eulerian path', function () {
+      const expected: Array<GraphNode> = [
+        new GraphNode('Node_0'),
+        new GraphNode('Node_1'),
+        new GraphNode('Node_2'),
+        new GraphNode('Node_1'),
+        new GraphNode('Node_3'),
+        new GraphNode('Node_4'),
+      ]
+      for (let i = 0; i < expected.length - 1; i++) {
+        graph.addEdge(expected[i], expected[i + 1], {
+          directed: true,
+        })
+      }
+
+      const path = eulerianPath(graph)
+
+      let result = expected.length === path.length
+      for (let i = 0; i < expected.length; i++) {
+        result = result && expected[i].isEqual(path[i])
+      }
+      expect(result).equal(true)
+    })
+  })
+
+  describe('Cycle', function () {
+    it('Should return empty cycle if graph is empty', function () {
+      const expected: Array<GraphNode> = []
+
+      const result = eulerianCycle(graph)
+
+      expect(result).deep.equal(expected)
+    })
+
+    it('Should return empty cycle when graph does not has eulerian cycle', function () {
+      const expected: Array<GraphNode> = []
+      graph.addEdge('Node_1', 'Node_2', {
+        directed: true,
+      })
+      graph.addEdge('Node_1', 'Node_3')
+      graph.addEdge('Node_4', 'Node_3', {
+        directed: true,
+      })
+      graph.addEdge('Node_4', 'Node_2', {
+        directed: true,
+      })
+
+      const result = eulerianCycle(graph)
+
+      expect(result).deep.equal(expected)
+    })
+
+    it('Should return eulerian cycle', function () {
+      const expected: Array<GraphNode> = [
+        new GraphNode('Node_3'),
+        new GraphNode('Node_4'),
+        new GraphNode('Node_2'),
+        new GraphNode('Node_1'),
+        new GraphNode('Node_3'),
+      ]
+      graph.addEdge('Node_1', 'Node_2')
+      graph.addEdge('Node_2', 'Node_4')
+      graph.addEdge('Node_4', 'Node_3')
+      graph.addEdge('Node_3', 'Node_1')
+
+      const path = eulerianCycle(graph)
+
+      let result = expected.length === path.length
+      for (let i = 0; i < expected.length; i++) {
+        result = result && expected[i].isEqual(path[i])
+      }
+      expect(result).equal(true)
+    })
+  })
+})
