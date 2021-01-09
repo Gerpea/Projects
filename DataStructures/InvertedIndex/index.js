235956eed5ab38bc62df1dece12ec5ea39489115
gerpea <german.tcyganov@gmail.com>
2021-01-09 04:27:12 -0500

    file selection

--- DataStructures/InvertedIndex/index.js
+++ DataStructures/InvertedIndex/index.js
@@ -1,11 +1,87 @@
-;(() => {
-  const btn = document.getElementById('add-file')
-  const inputFiles = document.getElementById('input-files')
-  let files = new Set()
+class TextFile {
+  constructor(node, file) {
+    this.node = node
+    this.file = file
+  }
+}
 
-  btn.addEventListener('click', () => {
+class Files {
+  constructor() {
+    this.files = new Map()
+    this.listeners = []
+  }
+
+  addListener(listener) {
+    this.listeners.push(listener)
+  }
+
+  notifyListeners() {
+    this.listeners.forEach((listener) => {
+      listener?.apply(this, [this.files])
+    })
+  }
+
+  addFile(node, value) {
+    this.files.set(value, node)
+    this.notifyListeners()
+  }
+
+  deleteFile(value) {
+    this.files.delete(value)
+    this.notifyListeners()
+  }
+
+  getFileByValue(value) {
+    return this.files.get(value)
+  }
+
+  getFileByNode(node) {
+    let result = undefined
+    this.files.forEach(function (value, n) {
+      if (node === value) {
+        result = n
+        return n
+      }
+    })
+    return result
+  }
+}
+
+let files = new Files()
+let selected = new Files()
+
+;(() => {
+  const addBtn = document.getElementById('add-file')
+  const removeBtn = document.getElementById('delete-file')
+  const inputFiles = document.getElementById('input-files')
+
+  selected.addListener(function (selectedFiles) {
+    if (selectedFiles.size > 0) {
+      removeBtn.style = 'display: block'
+    } else {
+      removeBtn.style = 'display: none'
+    }
+  })
+
+  let i = 0
+  addBtn.addEventListener('click', () => {
     const newFile = document.createElement('div')
-    files.add(newFile)
+    inputFiles.appendChild(newFile)
+
+    newFile.addEventListener('click', (event) => {
+      event.target.classList.toggle('files-list__el--selected')
+      const file = files.getFileByNode(event.target)
+      if (file !== undefined) {
+        if (selected.getFileByValue(file)) {
+          selected.deleteFile(file)
+        } else {
+          selected.addFile(event.target, file)
+        }
+      }
+    })
+
     newFile.className = 'files-list__el'
+
+    files.addFile(newFile, i++)
   })
 })()
