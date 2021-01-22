dba79e2247cad2c96cc7f7c0dec7df171370ee48
gerpea <german.tcyganov@gmail.com>
2021-01-22 12:36:42 -0500

    add airbnb eslitn

--- DataStructures/InvertedIndex/client/src/api.js
+++ DataStructures/InvertedIndex/client/src/api.js
@@ -1,66 +1,68 @@
-import axios from 'axios'
+import axios from 'axios';
 
-const serverAddress = process.env.SERVER_ADDRESS || '127.0.0.1:3000'
-const apiRoute = process.env.API_ROUTE || 'api'
+const serverAddress = process.env.SERVER_ADDRESS || '127.0.0.1:3000';
+const apiRoute = process.env.API_ROUTE || 'api';
 
 async function fetchFileById(id) {
-  return await axios.get(`http://${serverAddress}/${apiRoute}/file/${id}`)
+  return axios.get(`http://${serverAddress}/${apiRoute}/file/${id}`);
 }
 
 async function sendFile(file, progressListener) {
-  const formData = new FormData()
-  formData.append('file', file)
-  return await axios
+  const formData = new FormData();
+  formData.append('file', file);
+  return axios
     .post(`http://${serverAddress}/${apiRoute}/files`, formData, {
       headers: {
         'Content-Type': 'multipart/form-data',
         'Access-Control-Allow-Origin': '*',
       },
       onUploadProgress: (progressEvent) => {
-        const percentComleted = Math.round((progressEvent.loaded * 100) / progressEvent.total)
-        progressListener?.call(null, percentComleted)
+        const percentComleted = Math.round(
+          (progressEvent.loaded * 100) / progressEvent.total
+        );
+        progressListener?.call(null, percentComleted);
       },
     })
     .then(() => true)
-    .catch(() => false)
+    .catch(() => false);
 }
 
 function getFileUrl(file) {
-  return `http://${serverAddress}/files/${file.id}`
+  return `http://${serverAddress}/files/${file.id}`;
 }
 
 class SearchApi {
   constructor() {
-    this._connect()
-    this._listeners = []
-    this._lastRequestTrigger = undefined
+    this.connect();
+    this.listeners = [];
+    this.lastRequestTrigger = undefined;
   }
 
   addListener(listener) {
-    this._listeners.push(listener)
+    this.listeners.push(listener);
   }
 
   searchFiles(value) {
-    this._lastRequestTrigger = value
-    this._socket.send(value)
+    this.lastRequestTrigger = value;
+    this.socket.send(value);
   }
 
-  _connect() {
-    this._socket = new WebSocket(`ws://${serverAddress}/${apiRoute}/search`)
-    this._socket.addEventListener('message', (event) => {
-      const data = JSON.parse(event.data)
-      if (data.trigger === this._lastRequestTrigger) {
-        for (let listener of this._listeners) {
-          listener(data.files)
-        }
+  connect() {
+    this.socket = new WebSocket(`ws://${serverAddress}/${apiRoute}/search`);
+    this.socket.addEventListener('message', (event) => {
+      const data = JSON.parse(event.data);
+      if (data.trigger === this.lastRequestTrigger) {
+        this.listeners.forEach((listener) => {
+          listener(data.files);
+        });
       }
-    })
-    this._socket.addEventListener('close', () => {
+    });
+    this.socket.addEventListener('close', () => {
       setTimeout(() => {
-        this._connect()
-      }, 1000)
-    })
+        this.connect();
+      }, 1000);
+    });
   }
 }
 
-export { fetchFileById, sendFile, SearchApi, getFileUrl }
+export { fetchFileById, sendFile, SearchApi, getFileUrl };
--- DataStructures/InvertedIndex/client/src/utils.js
+++ DataStructures/InvertedIndex/client/src/utils.js
@@ -1,64 +1,68 @@
-import { v4 as uuidV4 } from 'uuid'
-import { fetchFileById } from './api'
-
-function showIn(area, filesId, onClick) {
-  const lists = area.getElementsByClassName('list')
-  if (!lists) {
-    return
-  }
-
-  const listNode = new Map()
-
-  for (let list of lists) {
-    while (list.firstChild) {
-      list.removeChild(list.firstChild)
-    }
-
-    for (let fileId of filesId) {
-      const fileNode = createFileNode(onClick)
-      list.appendChild(fileNode)
-      listNode.set(fileId, fileNode)
-    }
-  }
-
-  listNode.forEach(async (node, fileId) => {
-    await updateFileCard(fileId, node, onClick)
-  })
-}
+import { v4 as uuidV4 } from 'uuid';
+import { fetchFileById } from './api';
 
 function createFileNode() {
-  const fileNode = document.createElement('div')
-  fileNode.className = 'list__el card'
-  fileNode.id = `${uuidV4()}`
+  const fileNode = document.createElement('div');
+  fileNode.className = 'list__el card';
+  fileNode.id = `${uuidV4()}`;
 
-  return fileNode
+  return fileNode;
 }
 
 async function updateFileCard(fileId, fileNode, onClick) {
-  const fileTop = document.createElement('div')
-  fileTop.className = 'card__top'
-  const fileTitle = document.createElement('div')
-  fileTitle.className = 'card__title'
-  const fileTime = document.createElement('div')
-  fileTime.className = 'card__time'
-  const fileContent = document.createElement('div')
-  fileContent.className = 'card__content'
+  const fileTop = document.createElement('div');
+  fileTop.className = 'card__top';
+  const fileTitle = document.createElement('div');
+  fileTitle.className = 'card__title';
+  const fileTime = document.createElement('div');
+  fileTime.className = 'card__time';
+  const fileContent = document.createElement('div');
+  fileContent.className = 'card__content';
 
-  const file = (await fetchFileById(fileId)).data
-  fileTitle.innerText = file.name
-  fileTime.innerHTML = new Intl.DateTimeFormat(navigator.language || navigator.userLanguage, {
-    dateStyle: 'long',
-    timeStyle: 'short',
-  }).format(new Date(file.timestamp))
-  fileContent.innerText = file.description
+  const file = (await fetchFileById(fileId)).data;
+  fileTitle.innerText = file.name.trim();
+  fileTime.innerHTML = new Intl.DateTimeFormat(
+    navigator.language || navigator.userLanguage,
+    {
+      dateStyle: 'long',
+      timeStyle: 'short',
+    }
+  ).format(new Date(file.timestamp));
+  fileContent.innerText = file.description.trim();
 
-  fileTop.appendChild(fileTitle)
-  fileTop.appendChild(fileTime)
+  fileTop.appendChild(fileTitle);
+  fileTop.appendChild(fileTime);
 
-  fileNode.appendChild(fileTop)
-  fileNode.appendChild(fileContent)
+  fileNode.appendChild(fileTop);
+  fileNode.appendChild(fileContent);
 
-  fileNode.onclick = () => onClick?.call(this, file)
+  fileNode.addEventListener('click', () => {
+    onClick?.call(this, file);
+  });
 }
 
-export { showIn }
+function showIn(area, filesId, onClick) {
+  const lists = area.getElementsByClassName('list');
+  if (!lists) {
+    return;
+  }
+
+  const listNode = new Map();
+
+  lists.forEach((list) => {
+    while (list.firstChild) {
+      list.removeChild(list.firstChild);
+    }
+    filesId.forEach((fileId) => {
+      const fileNode = createFileNode(onClick);
+      list.appendChild(fileNode);
+      listNode.set(fileId, fileNode);
+    });
+  });
+
+  listNode.forEach((node, fileId) => {
+    updateFileCard(fileId, node, onClick);
+  });
+}
+
+export default showIn;
